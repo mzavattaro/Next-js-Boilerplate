e7d12769c1442c21a81d0a2b3d99a69404fc3b0c
mzavattaro <mwzavattaro@icloud.com>
2024-07-21 12:19:54 +1000

    docs(update code comment): update code comment inside of the posthog provider for better clarity

--- src/providers.js
+++ src/providers.js
@@ -8,7 +8,7 @@ import { PostHogProvider } from 'posthog-js/react';
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
+    person_profiles: 'always', // or 'identified_only' to create profiles for only identified users
   });
 }
 
